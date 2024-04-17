51a8fff276c8df353537b57681053484b2b5a3eb
cjmpmd <71334484+cjmpmd@users.noreply.github.com>
2024-04-17 18:17:18 -0400

    Added troubleshooting and general convention naming. Pending to complete the function

--- troubleshoot_k8_main.sh
+++ troubleshoot_k8_main.sh
@@ -1,9 +1,16 @@
-sudo kubeadm reset 
+#git clone https://github.com/cjmpmd/k8_v28.git
 
-sudo systemctl stop kubelet
-sudo systemctl restart kubelet
 
+sudo $HOME/.kube/config file
 sudo rm /etc/kubernetes/manifests/kube-apiserver.yaml
 sudo rm /etc/kubernetes/manifests/kube-controller-manager.yaml
 sudo rm /etc/kubernetes/manifests/etcd.yaml
 sudo rm /etc/kubernetes/manifests/kube-scheduler.yaml
+sudo kubeadm reset 
+
+sudo systemctl stop kubelet
+
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+sudo systemctl restart kubelet
+sudo systemctl daemon-reload && sudo systemctl restart docker
